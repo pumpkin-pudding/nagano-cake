613e1f50fd7e155891f394f55e9573b0517cf92f
EC2 Default User <ec2-user@ip-172-31-42-207.ap-northeast-1.compute.internal>
2023-10-19 06:09:03 +0000

    add admin-sign_in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2023_10_19_014157) do
-ActiveRecord::Schema.define(version: 2023_10_18_111536) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
