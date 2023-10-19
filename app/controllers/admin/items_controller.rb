6d442d6f73e2ade275e8fb153ac8655fac4eb11e
EC2 Default User <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-10-19 04:10:56 +0000

    注文履歴詳細画面編集中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,5 +4,11 @@ class Admin::ItemsController < ApplicationController
  end
  
  def edit
+  @item = Item.find(params[:id])
+ end
+ 
+ private
+ def item_params
+  params.require(:item).permit(:genre_id, :name, :text, :non_taxed_price, :is_active)
  end 
 end
