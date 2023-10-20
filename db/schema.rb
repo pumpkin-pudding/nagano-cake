1f4b3ae46c98ee31150a118dce0f13aa71500417
EC2 Default User <ec2-user@ip-172-31-13-168.ap-northeast-1.compute.internal>
2023-10-20 01:33:32 +0000

    マイグレションにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -105,6 +105,11 @@ ActiveRecord::Schema.define(version: 2023_10_19_014157) do
   end
 
   create_table "order_details", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "price", null: false
+    t.integer "amount", null: false
+    t.integer "creating_status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
