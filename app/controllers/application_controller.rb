6158930e7e2e4381235b18ef1cbda4383dfeb949
EC2 Default User <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-10-25 17:31:39 +0000

    add controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+ before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+        admin_path
+    when Customer
+        root_path
+    end 
+  end 
+    
+  
+  def after_sign_out_path_for(resource)
+    case resource
+    when Admin
+        new_admin_session_path
+    when Customer
+        root_path
+    end 
+  end 
+        
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:email, :encrypted_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :phone_number, :zip_code, :address, :first_name_kana, :last_name_kana, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:email, :phone_number, :zip_code, :address, :first_name_kana, :last_name_kana, :first_name, :last_name])
+  end 
 end
