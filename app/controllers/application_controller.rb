c5dcf507a58ce8f30c21ed0e24735dc225bc3356
EC2 Default User <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-10-26 07:40:35 +0000

    ログアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,48 +1,35 @@
 class ApplicationController < ActionController::Base
  #before_action :configure_permitted_parameters, if: :devise_controller?
-before_action :authenticate_admin!, if: :admin_url
+# before_action :authenticate_admin!, if: :admin_url
 
-  private
-  def admin_url
-    request.fullpath.starts_with?('/admin')
-  end
+#   private
+#   def admin_url
+#     request.fullpath.starts_with?('/admin')
+#   end
 
-
+   def after_sign_in_path_for(resource)
+     case resource
+     when Admin
+         admin_root_path
+     when Customer
+         root_path
+     end
+   end
+   
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
-      new_admin_session_path
-    else
-      root_path
+       new_admin_session_path
+    elsif resource_or_scope == :customer
+        root_path
     end
   end
-  
-  
 
-
-#   def after_sign_in_path_for(resource)
-#     case resource
-#     when Admin
-#         admin_path
-#     when Customer
-#         root_path
-#     end
+#   private
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.permit(:sign_in, keys:[:email, :encrypted_password])
+#     devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :phone_number, :zip_code, :address, :first_name_kana, :last_name_kana, :first_name, :last_name])
+#     devise_parameter_sanitizer.permit(:account_update, keys:[:email, :phone_number, :zip_code, :address, :first_name_kana, :last_name_kana, :first_name, :last_name])
 #   end
 
-#   def after_sign_out_path_for(resource)
-#     case resource
-#     when Admin
-#         new_admin_session_path
-#     when Customer
-#         root_path
-#     end
-#   end
-
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys:[:email, :encrypted_password])
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :phone_number, :zip_code, :address, :first_name_kana, :last_name_kana, :first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys:[:email, :phone_number, :zip_code, :address, :first_name_kana, :last_name_kana, :first_name, :last_name])
-  end
-
-end
+ end
 
