492b6758b1bf5d320f32bfcb1d9d57984f9b02b0
EC2 Default User <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-10-26 09:20:30 +0000

    public/ordersのコントローラー修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -53,6 +53,15 @@ class Public::OrdersController < ApplicationController
 
 
  end
+ 
+ def show
+      @order = Order.find(params[:id])
+      @order_details= OrderDetail.where(order_id: @order.id)
+ end
+ 
+ def thanks
+ end 
+ 
    private
 
     def order_params
