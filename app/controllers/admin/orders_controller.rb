6bd555bc25e3546a736104fc9d2141339d18dcb4
EC2 Default User <ec2-user@ip-172-31-13-168.ap-northeast-1.compute.internal>
2023-10-27 03:32:34 +0000

    オーダー画面修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,6 +12,13 @@ end
 def thanks
 end
 
+def update
+  @order = Order.find(params[:id])
+  @order.update(order_params)
+  @order_details = @order.order_details
+  redirect_to admin_order_path
+end
+
 def order_params
  params.require(:order).permit(:creating_status)
 end
